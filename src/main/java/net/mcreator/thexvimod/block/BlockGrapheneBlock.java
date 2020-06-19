76020e2e79fc89de22565ea7f0a84cca79869dd6
synth <synth@192.168.1.1>
2020-06-19 19:36:11 +0100

    yes

--- src/main/java/net/mcreator/thexvimod/block/BlockGrapheneBlock.java
+++ src/main/java/net/mcreator/thexvimod/block/BlockGrapheneBlock.java
@@ -1,12 +1,27 @@
 
 package net.mcreator.thexvimod.block;
 
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
 @ElementsTheXVImod.ModElement.Tag
 public class BlockGrapheneBlock extends ElementsTheXVImod.ModElement {
-
 	@GameRegistry.ObjectHolder("thexvimod:grapheneblock")
 	public static final Block block = null;
-
 	public BlockGrapheneBlock(ElementsTheXVImod instance) {
 		super(instance, 60);
 	}
@@ -22,32 +37,23 @@ public class BlockGrapheneBlock extends ElementsTheXVImod.ModElement {
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation("thexvimod:grapheneblock", "inventory"));
-
 	}
-
 	public static class BlockCustom extends Block {
-
 		public BlockCustom() {
 			super(Material.IRON);
-
 			setUnlocalizedName("grapheneblock");
 			setSoundType(SoundType.METAL);
-
 			setHarvestLevel("pickaxe", 2);
-
 			setHardness(3F);
 			setResistance(20F);
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-
 		}
 
 		@Override
 		public int tickRate(World world) {
 			return 0;
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/thexvimod/block/BlockTripleCompressedCarbon.java
+++ src/main/java/net/mcreator/thexvimod/block/BlockTripleCompressedCarbon.java
@@ -1,12 +1,27 @@
 
 package net.mcreator.thexvimod.block;
 
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
 @ElementsTheXVImod.ModElement.Tag
 public class BlockTripleCompressedCarbon extends ElementsTheXVImod.ModElement {
-
 	@GameRegistry.ObjectHolder("thexvimod:triplecompressedcarbon")
 	public static final Block block = null;
-
 	public BlockTripleCompressedCarbon(ElementsTheXVImod instance) {
 		super(instance, 61);
 	}
@@ -22,32 +37,23 @@ public class BlockTripleCompressedCarbon extends ElementsTheXVImod.ModElement {
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation("thexvimod:triplecompressedcarbon", "inventory"));
-
 	}
-
 	public static class BlockCustom extends Block {
-
 		public BlockCustom() {
 			super(Material.ROCK);
-
 			setUnlocalizedName("triplecompressedcarbon");
 			setSoundType(SoundType.STONE);
-
 			setHarvestLevel("pickaxe", 2);
-
 			setHardness(5F);
 			setResistance(20F);
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-
 		}
 
 		@Override
 		public int tickRate(World world) {
 			return 0;
 		}
-
 	}
-
 }

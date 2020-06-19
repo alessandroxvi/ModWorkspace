b1e8b98a4b489c6a2b0823facfdc61656673a0b1
etien <etien@who-is-joe.lan>
2020-06-19 22:04:51 +0100

    opal gem

---
+++ src/main/java/net/mcreator/thexvimod/item/ItemOpla.java
@@ -0,0 +1,66 @@
+
+package net.mcreator.thexvimod.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
+@ElementsTheXVImod.ModElement.Tag
+public class ItemOpla extends ElementsTheXVImod.ModElement {
+	@GameRegistry.ObjectHolder("thexvimod:opla")
+	public static final Item block = null;
+	public ItemOpla(ElementsTheXVImod instance) {
+		super(instance, 67);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("thexvimod:opla", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("opla");
+			setRegistryName("opla");
+			setCreativeTab(CreativeTabs.MATERIALS);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}

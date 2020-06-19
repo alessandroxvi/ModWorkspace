beecccbb07d8b6f1475f6329bd37d5ee13cfe42c
kobe <kobe@DESKTOP-M9ENT8K.Home>
2020-06-19 15:44:19 +0100

    reinforecedcobble1

---
+++ src/main/java/net/mcreator/thexvimod/block/BlockReinforcedcobblestone.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.thexvimod.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
+@ElementsTheXVImod.ModElement.Tag
+public class BlockReinforcedcobblestone extends ElementsTheXVImod.ModElement {
+	@GameRegistry.ObjectHolder("thexvimod:reinforcedcobblestone")
+	public static final Block block = null;
+	public BlockReinforcedcobblestone(ElementsTheXVImod instance) {
+		super(instance, 56);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("reinforcedcobblestone"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("thexvimod:reinforcedcobblestone", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public BlockCustom() {
+			super(Material.ROCK);
+			setUnlocalizedName("reinforcedcobblestone");
+			setSoundType(SoundType.STONE);
+			setHarvestLevel("pickaxe", 2);
+			setHardness(5F);
+			setResistance(225F);
+			setLightLevel(0F);
+			setLightOpacity(255);
+			setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		}
+
+		@Override
+		public int tickRate(World world) {
+			return 0;
+		}
+	}
+}

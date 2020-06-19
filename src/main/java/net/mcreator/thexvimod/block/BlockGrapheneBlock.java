ec0b15371da161def1605875acf715cf7f3172ea
synth <synth@192.168.1.1>
2020-06-19 19:05:14 +0100

    grapheneblock, triple compressed

---
+++ src/main/java/net/mcreator/thexvimod/block/BlockGrapheneBlock.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.thexvimod.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
+@ElementsTheXVImod.ModElement.Tag
+public class BlockGrapheneBlock extends ElementsTheXVImod.ModElement {
+	@GameRegistry.ObjectHolder("thexvimod:grapheneblock")
+	public static final Block block = null;
+	public BlockGrapheneBlock(ElementsTheXVImod instance) {
+		super(instance, 60);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("grapheneblock"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("thexvimod:grapheneblock", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public BlockCustom() {
+			super(Material.IRON);
+			setUnlocalizedName("grapheneblock");
+			setSoundType(SoundType.METAL);
+			setHarvestLevel("pickaxe", 2);
+			setHardness(3F);
+			setResistance(20F);
+			setLightLevel(0F);
+			setLightOpacity(255);
+			setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		}
+
+		@Override
+		public int tickRate(World world) {
+			return 0;
+		}
+	}
+}

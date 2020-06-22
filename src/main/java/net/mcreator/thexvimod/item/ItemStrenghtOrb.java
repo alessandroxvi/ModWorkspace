037b5d7d6cea68e484a0f890b06679a34fc10b1a
kobe <kobe@DESKTOP-M9ENT8K.Home>
2020-06-22 11:20:45 +0100

    strength orb

---
+++ src/main/java/net/mcreator/thexvimod/item/ItemStrenghtOrb.java
@@ -0,0 +1,77 @@
+
+package net.mcreator.thexvimod.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.thexvimod.procedure.ProcedureStrenghtOrbItemInHandTick;
+import net.mcreator.thexvimod.ElementsTheXVImod;
+
+@ElementsTheXVImod.ModElement.Tag
+public class ItemStrenghtOrb extends ElementsTheXVImod.ModElement {
+	@GameRegistry.ObjectHolder("thexvimod:strenghtorb")
+	public static final Item block = null;
+	public ItemStrenghtOrb(ElementsTheXVImod instance) {
+		super(instance, 117);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("thexvimod:strenghtorb", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("strenghtorb");
+			setRegistryName("strenghtorb");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void onUpdate(ItemStack itemstack, World world, Entity entity, int slot, boolean par5) {
+			super.onUpdate(itemstack, world, entity, slot, par5);
+			int x = (int) entity.posX;
+			int y = (int) entity.posY;
+			int z = (int) entity.posZ;
+			if (entity instanceof EntityLivingBase && ((EntityLivingBase) entity).getHeldItemMainhand().equals(itemstack)) {
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				ProcedureStrenghtOrbItemInHandTick.executeProcedure($_dependencies);
+			}
+		}
+	}
+}
